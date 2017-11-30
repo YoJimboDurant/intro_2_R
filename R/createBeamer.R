dde03cbc4db74ad345923e77c08b46850de29463
James Durant <hzd3@cdc.gov>
2017-11-30 15:34:24 -0500

    updating to R talk

--- R/createBeamer.R
+++ R/createBeamer.R
@@ -4,6 +4,6 @@ source("./R/RmdFlip.R")
 source("./R/createLessonMaster.R")
 
 x <- list.files("Rmd_lessons", pattern = "[.]Rmd", full.names = TRUE)
-
+x <- x[grepl("RUG_Talk_2.Rmd", x)]
 createLessonMaster(x, type="beamer")
 render("Master_beamer.Rmd")
