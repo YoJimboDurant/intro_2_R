a907395fbc8ee019a5224b16b6446140b76d5d4f
James Durant <hzd3@cdc.gov>
2017-12-04 15:33:11 -0500

    commmit 12/4/2017

--- R/createWord.R
+++ R/createWord.R
@@ -4,5 +4,6 @@ source("./R/RmdFlip.R")
 source("./R/createLessonMaster.R")
 
 x <- list.files("Rmd_lessons", pattern = "[.]Rmd", full.names = TRUE)
+x <- x[grepl("RUG_Talk_2.Rmd", x)]
 createLessonMaster(x, type="article", headFile = "./Rmd_templates/word.Rmd")
 render("Master_article.Rmd")
